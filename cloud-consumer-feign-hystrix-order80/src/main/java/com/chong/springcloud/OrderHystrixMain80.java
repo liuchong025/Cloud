084f3d324a19aeb7be1a5a8758dd06a7b1201d84
chong.b.liu <liuchong8865236>
2021-09-16 17:44:04 +0800

    新增feign-hystrix模块

---
+++ cloud-consumer-feign-hystrix-order80/src/main/java/com/chong/springcloud/OrderHystrixMain80.java
@@ -0,0 +1,17 @@
+package com.chong.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+/**
+ * @author chong.b.liu
+ */
+@SpringBootApplication
+@EnableFeignClients
+public class OrderHystrixMain80 {
+    public static void main(String[] args) {
+        SpringApplication.run(OrderHystrixMain80.class,args);
+    }
+
+}
