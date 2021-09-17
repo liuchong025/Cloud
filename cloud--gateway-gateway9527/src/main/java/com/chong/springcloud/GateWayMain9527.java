25e3444df2c401aa8967fa17311756ed74a85bd5
chong.b.liu <liuchong8865236>
2021-09-17 15:11:19 +0800

    1

---
+++ cloud--gateway-gateway9527/src/main/java/com/chong/springcloud/GateWayMain9527.java
@@ -0,0 +1,16 @@
+package com.chong.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @author chong.b.liu
+ */
+@SpringBootApplication
+@EnableEurekaClient
+public class GateWayMain9527 {
+    public static void main(String[] args) {
+        SpringApplication.run(GateWayMain9527.class,args);
+    }
+}
